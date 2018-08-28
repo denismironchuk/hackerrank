25d9016ad57a88a4666b15d5955f60342e2a06d9
Denis Mironchuk <denis@mironchuk.in.ua>
2018-08-28 22:43:33 +0300

    story of a tree final impl

--- hackerrank/src/StoryOfTree.java
+++ hackerrank/src/StoryOfTree.java
@@ -1,16 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class StoryOfTree {
     static class Node {
         private int num;
         private List<Node> neighbours = new ArrayList<>();
-        private int expectedParent;
-        private int expectedChild;
+        private Set<Integer> expectedParents = new HashSet<>();
+        private Set<Integer> expectedChildren = new HashSet<>();
+        private int rightGuesses = 0;
 
         public Node(final int num) {
             this.num = num;
@@ -28,20 +27,32 @@ public class StoryOfTree {
             return neighbours;
         }
 
-        public int getExpectedParent() {
-            return expectedParent;
+        public void addExpectedParent(final int expectedParent) {
+            this.expectedParents.add(expectedParent);
         }
 
-        public void setExpectedParent(final int expectedParent) {
-            this.expectedParent = expectedParent;
+        public void addExpectedChild(final int expectedChild) {
+            this.expectedChildren.add(expectedChild);
         }
 
-        public int getExpectedChild() {
-            return expectedChild;
+        public Set<Integer> getExpectedParents() {
+            return expectedParents;
         }
 
-        public void setExpectedChild(final int expectedChild) {
-            this.expectedChild = expectedChild;
+        public Set<Integer> getExpectedChildren() {
+            return expectedChildren;
+        }
+
+        public int getRightGuesses() {
+            return rightGuesses;
+        }
+
+        public void setRightGuesses(int rightGuesses) {
+            this.rightGuesses = rightGuesses;
+        }
+
+        public void addRightGuesses(int rightGuesses) {
+            this.rightGuesses += rightGuesses;
         }
     }
 
@@ -74,17 +85,36 @@ public class StoryOfTree {
                 int u = Integer.parseInt(pairTkn.nextToken()) - 1;
                 int v = Integer.parseInt(pairTkn.nextToken()) - 1;
 
-                nodes[v].setExpectedParent(u);
-                nodes[u].setExpectedChild(v);
+                nodes[v].addExpectedParent(u);
+                nodes[u].addExpectedChild(v);
             }
 
-            Node start = null;
+            nodes[0].setRightGuesses(countInclusiveClasses(nodes[0], new int[n]));
+            countRightGuesses(nodes[0], new int[n]);
 
-            for (int i = 0; i < n && start == null; i++) {
-                if (nodes[i].getNeighbours().size() == 1) {
-                    start = nodes[i];
+            int successCnt = 0;
+            for (int i = 0; i< n; i++) {
+                if (nodes[i].getRightGuesses() >= k) {
+                    successCnt++;
                 }
             }
+
+            if (successCnt == 0) {
+                System.out.println("0/1");
+            } else {
+                int gcd = gcd(n, successCnt);
+                System.out.printf("%d/%d\n", successCnt / gcd, n/ gcd);
+            }
+        }
+    }
+
+    private static int gcd(int a, int b) {
+        int ost = a % b;
+
+        if (ost != 0) {
+            return gcd(b, ost);
+        } else {
+            return b;
         }
     }
 
@@ -94,10 +124,33 @@ public class StoryOfTree {
 
         for (Node neighbour : nd.getNeighbours()) {
             if (processed[neighbour.getNum()] == 0) {
+                if (neighbour.getExpectedParents().contains(nd.num)) {
+                    result += 1;
+                }
                 result += countInclusiveClasses(neighbour, processed);
             }
         }
 
         return result;
+    }
+
+    private static void countRightGuesses(Node nd, int[] processed) {
+        processed[nd.getNum()] = 1;
+
+        for (Node neighbour : nd.getNeighbours()) {
+            if (processed[neighbour.getNum()] == 0) {
+                neighbour.addRightGuesses(nd.getRightGuesses());
+
+                if (neighbour.getExpectedParents().contains(nd.getNum())) {
+                    neighbour.addRightGuesses(-1);
+                }
+
+                if (nd.getExpectedParents().contains(neighbour.getNum())) {
+                    neighbour.addRightGuesses(1);
+                }
+
+                countRightGuesses(neighbour, processed);
+            }
+        }
     }
 }
