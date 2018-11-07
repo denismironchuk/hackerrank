1ac6acbde3c253b42d5c911c5e206941234ae2b5
Denis Mironchuk <denis_mironchuk@epam.com>
2018-11-07 12:54:34 +0200

    city of blinding lights

--- hackerrank/src/CityOfBlindingLights.java
+++ hackerrank/src/CityOfBlindingLights.java
@@ -19,7 +19,36 @@ public class CityOfBlindingLights {
 
         for (int i = 0; i < m; i++) {
             StringTokenizer edgeTkn = new StringTokenizer(br.readLine());
+            int v1 = Integer.parseInt(edgeTkn.nextToken()) - 1;
+            int v2 = Integer.parseInt(edgeTkn.nextToken()) - 1;
+            int r = Integer.parseInt(edgeTkn.nextToken());
 
+            dists[v1][v2] = r;
         }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    dists[i][j] = Math.min(dists[i][j], dists[i][k] + dists[k][j]);
+                }
+            }
+        }
+
+        StringBuilder res = new StringBuilder();
+
+        int q = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < q; i++) {
+            StringTokenizer query = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(query.nextToken()) - 1;
+            int b = Integer.parseInt(query.nextToken()) - 1;
+            if (dists[a][b] >= Integer.MAX_VALUE) {
+                res.append("-1\n");
+            } else {
+                res.append(dists[a][b]).append("\n");
+            }
+        }
+
+        System.out.println(res.toString());
     }
 }
