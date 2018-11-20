05e30e9d3f4fcbb953136090234c25a2c2b376d6
Denis Mironchuk <denis_mironchuk@epam.com>
2018-11-20 14:02:21 +0200

    journey scheduling final

--- hackerrank/src/JourneySheduling.java
+++ hackerrank/src/JourneySheduling.java
@@ -5,6 +5,8 @@ import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 
 /**
  * Created by Denis_Mironchuk on 11/19/2018.
@@ -13,6 +15,8 @@ public class JourneySheduling {
     static class Node {
         private int num;
         private Set<Node> neighbours = new HashSet<>();
+        private long dist1 = 0;
+        private long dist2 = 0;
 
         public Node(final int num) {
             this.num = num;
@@ -29,10 +33,26 @@ public class JourneySheduling {
         public Set<Node> getNeighbours() {
             return neighbours;
         }
+
+        public long getDist1() {
+            return dist1;
+        }
+
+        public long getDist2() {
+            return dist2;
+        }
+
+        public void setDist1(final long dist1) {
+            this.dist1 = dist1;
+        }
+
+        public void setDist2(final long dist2) {
+            this.dist2 = dist2;
+        }
     }
     public static void main(String[] args) throws IOException {
-        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedReader br = new BufferedReader(new FileReader("D:/journey.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedReader br = new BufferedReader(new FileReader("D:/journey.txt"));
         StringTokenizer line1Tkn = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(line1Tkn.nextToken());
@@ -55,26 +75,41 @@ public class JourneySheduling {
 
         StringBuilder res = new StringBuilder();
 
+        Response rsp1 = findMostDistant(nodes[0], new int[n]);
+        Node nd1 = rsp1.node;
+        Response rsp2 = findMostDistant(nd1, new int[n]);
+        Node nd2 = rsp2.node;
+        setDistance(nd1, new int[n], 0, Node::setDist1, Node::getDist1);
+        setDistance(nd2, new int[n], 0, Node::setDist2, Node::getDist2);
+
+        long maxDist = nd2.getDist1();
+
         for (int i = 0; i < m; i++) {
             StringTokenizer queryTkn = new StringTokenizer(br.readLine());
             int v = Integer.parseInt(queryTkn.nextToken()) - 1;
             long k = Long.parseLong(queryTkn.nextToken());
 
-            long result = 0;
-
-            Response rsp = findMostDistant(nodes[v], new int[n]);
-
-            result += rsp.dist;
+            long result = Math.max(nodes[v].getDist1(), nodes[v].getDist2());
 
             if (k > 1) {
-                Response resp2 = findMostDistant(rsp.node, new int[n]);
-                result += resp2.dist * (k - 1);
+                result += maxDist * (k - 1);
             }
 
             res.append(result).append("\n");
         }
 
         System.out.println(res.toString());
+    }
+
+    private static void setDistance(Node nd, int[] processed, long dist, BiConsumer<Node, Long> distConsumer, Function<Node, Long> distSupplier) {
+        distConsumer.accept(nd, dist);
+        processed[nd.getNum()] = 1;
+
+        for (Node neigh : nd.getNeighbours()) {
+            if (processed[neigh.getNum()] == 0) {
+                setDistance(neigh, processed, distSupplier.apply(nd) + 1, distConsumer, distSupplier);
+            }
+        }
     }
 
     static class Response {
