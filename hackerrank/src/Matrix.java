e2d69b19b0d99fb2c6b8fadb7852f93ab04ce47d
Denis Mironchuk <denis@mironchuk.in.ua>
2018-11-24 21:41:50 +0200

    matrix final

--- hackerrank/src/Matrix.java
+++ hackerrank/src/Matrix.java
@@ -1,25 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Matrix {
     static class Node {
         private int num;
-        private Map<Node, Long> neighbours = new HashMap<>();
+        private Set<Edge> neighbours = new HashSet<>();
         private boolean isMachine = false;
 
         public Node(final int num) {
             this.num = num;
         }
 
-        public void addNeigh(Node neigh, Long time) {
-            neighbours.put(neigh, time);
+        public void addNeigh(Edge neigh) {
+            neighbours.add(neigh);
         }
 
-        public Map<Node, Long> getNeighbours() {
+        public Set<Edge> getNeighbours() {
             return neighbours;
         }
 
@@ -36,6 +34,46 @@ public class Matrix {
         }
     }
 
+    static class Edge implements Comparable<Edge> {
+        private int nd1;
+        private int nd2;
+        private int id;
+        private long time;
+
+        public Edge(int nd1, int nd2, int id, long time) {
+            this.nd1 = nd1;
+            this.nd2 = nd2;
+            this.id = id;
+            this.time = time;
+        }
+
+        public int getNd1() {
+            return nd1;
+        }
+
+        public int getNd2() {
+            return nd2;
+        }
+
+        public long getTime() {
+            return time;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            int timeCompare = Long.compare(time, o.getTime());
+            if (timeCompare == 0) {
+                return Integer.compare(id, o.getId());
+            } else {
+                return timeCompare;
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer line1Tkn = new StringTokenizer(br.readLine());
@@ -48,46 +86,72 @@ public class Matrix {
             nodes[i] = new Node(i);
         }
 
+        TreeSet<Edge> edges = new TreeSet<>();
+
         for (int i = 0; i < n - 1; i++) {
             StringTokenizer roadTkn = new StringTokenizer(br.readLine());
             int n1 = Integer.parseInt(roadTkn.nextToken());
             int n2 = Integer.parseInt(roadTkn.nextToken());
             long time = Long.parseLong(roadTkn.nextToken());
 
-            nodes[n1].addNeigh(nodes[n2], time);
-            nodes[n2].addNeigh(nodes[n1], time);
-        }
+            Edge ed = new Edge(n1, n2, i, time);
 
-        Node machine = null;
+            nodes[n1].addNeigh(ed);
+            nodes[n2].addNeigh(ed);
+
+            edges.add(ed);
+        }
 
         for (int i = 0; i < k; i++) {
             int machineId = Integer.parseInt(br.readLine());
-            machine = nodes[machineId];
-            machine.setMachine(true);
+            nodes[machineId].setMachine(true);
         }
 
-        System.out.println(getMinTime(machine, new int[n], 0));
+        Queue<Node> toRemoveQ = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            addNodeToQueue(nodes[i], toRemoveQ);
+        }
+
+        removeEges(toRemoveQ, edges, nodes);
+
+        long result = 0;
+
+        while (!edges.isEmpty()) {
+            Edge minTimeEdge = edges.pollFirst();
+            result += minTimeEdge.getTime();
+
+            Node n1 = nodes[minTimeEdge.nd1];
+            Node n2 = nodes[minTimeEdge.nd2];
+
+            n1.getNeighbours().remove(minTimeEdge);
+            n2.getNeighbours().remove(minTimeEdge);
+
+            addNodeToQueue(n1, toRemoveQ);
+            addNodeToQueue(n2, toRemoveQ);
+
+            removeEges(toRemoveQ, edges, nodes);
+        }
+
+        System.out.println(result);
     }
 
-    private static long getMinTime(Node nd, int[] processed, long minTime) {
-        long res = 0;
-
-        if (nd.isMachine()) {
-            res += minTime;
-            minTime = Long.MAX_VALUE;
+    private static void addNodeToQueue(Node nd, Queue<Node> toRemoveQ) {
+        if (!nd.isMachine() && nd.getNeighbours().size() == 1) {
+            toRemoveQ.add(nd);
         }
+    }
 
-        processed[nd.getNum()] = 1;
+    private static void removeEges(Queue<Node> toRemoveQ, TreeSet<Edge> edges, Node[] nodes) {
+        while (!toRemoveQ.isEmpty()) {
+            Node toRemove = toRemoveQ.poll();
+            Edge edToRemove = toRemove.getNeighbours().iterator().next();
+            edges.remove(edToRemove);
 
-        for (Map.Entry<Node, Long> entry : nd.getNeighbours().entrySet()) {
-            Node next = entry.getKey();
-            long time = entry.getValue();
+            Node neighbour = nodes[edToRemove.getNd1() == toRemove.getNum() ? edToRemove.getNd2() : edToRemove.getNd1()];
+            neighbour.getNeighbours().remove(edToRemove);
 
-            if (processed[next.getNum()] == 0) {
-                res += getMinTime(next, processed, Math.min(minTime, time));
-            }
+            addNodeToQueue(neighbour, toRemoveQ);
         }
-
-        return res;
     }
 }
