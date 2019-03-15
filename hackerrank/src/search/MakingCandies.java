c61bf0c7739d84d205c0106953b8ec4a3a2ffdb5
Denis Mironchuk <denis@mironchuk.in.ua>
2019-03-15 21:13:27 +0200

    making candies

--- hackerrank/src/search/MakingCandies.java
+++ hackerrank/src/search/MakingCandies.java
@@ -16,38 +16,63 @@ public class MakingCandies {
 
         long candies = 0;
         long passes = 0;
-
+        long leftCandies = 0;
         int iterations = 0;
 
-        while (candies < n) {
-            iterations++;
-            long locPasses = 1;
-            if (candies < p) {
-                locPasses = ((p - candies) / (m * w)) + ((p - candies) % (m * w) == 0 ? 0 : 1);
-            }
-            candies += locPasses * m * w;
-            passes += locPasses;
+        if (n <= p) {
+            passes = (n / (n * w)) + (n % (n * w) == 0 ? 0 : 1);
+        } else {
+            while (candies < n) {
+                iterations++;
+                //System.out.println(m + " " + w);
+                long locPasses = 1;
+                if (candies - leftCandies < p) {
+                    locPasses = ((p - candies + leftCandies) / (m * w)) + ((p - candies + leftCandies) % (m * w) == 0 ? 0 : 1);
+                }
+                candies += locPasses * m * w;
+                passes += locPasses;
+                //leftCandies += p;
 
-            if (candies >= n) {
-                break;
-            }
+                if (candies >= n) {
+                    break;
+                }
 
-            long buy = candies / p;
-            long remCandies = candies % p;
-            long newM = (m + w + buy) / 2;
-            long newW = (m + w + buy) % 2 == 0 ? (m + w + buy) / 2 : 1 + ((m + w + buy) / 2);
+                long buy = candies / p;
+                long remCandies = candies % p;
 
-            long newPasses = ((n - remCandies) / (newM * newW)) + ((n - remCandies) % (newM * newW) == 0 ? 0 : 1);
-            long oldPasses = ((n - candies) / (m * w)) + ((n - candies) % (m * w) == 0 ? 0 : 1);
+                long newM = m;
+                long newW = w;
 
-            if (newPasses <= oldPasses) {
-                candies = remCandies;
-                m = newM;
-                w = newW;
+                if (m > w) {
+                    newW = w + Math.min(buy, m - w);
+                    long rest = buy - newW + w;
+
+                    newM += (rest / 2);
+                    newW += (rest / 2) + ((rest % 2) == 0 ? 0 : 1);
+                } else if (w > m) {
+                    newM = m + Math.min(buy, w - m);
+                    long rest = buy - newM + m;
+
+                    newM += (rest / 2);
+                    newW += (rest / 2) + ((rest % 2) == 0 ? 0 : 1);
+                } else {
+                    newM += (buy / 2);
+                    newW += (buy / 2) + ((buy % 2) == 0 ? 0 : 1);
+                }
+
+                long newPasses = ((n - remCandies) / (newM * newW)) + ((n - remCandies) % (newM * newW) == 0 ? 0 : 1);
+                long oldPasses = ((n - candies) / (m * w)) + ((n - candies) % (m * w) == 0 ? 0 : 1);
+
+                if (newPasses <= oldPasses) {
+                    candies = remCandies;
+                    m = newM;
+                    w = newW;
+                    leftCandies = 0;
+                }
             }
         }
 
         System.out.println(passes);
-        System.out.println(iterations);
+        //System.out.println(iterations);
     }
 }
