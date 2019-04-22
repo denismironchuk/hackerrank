1d3a37dab08c6edd4fa66a88a3dc53581a07fdcc
denis_mironchuk <denis_mironchuk@epam.com>
2019-04-22 20:47:52 +0300

    Task scheduling segment tree approach

--- hackerrank/src/search/TaskSheduling.java
+++ hackerrank/src/search/TaskSheduling.java
@@ -3,6 +3,7 @@ package search;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class TaskSheduling {
@@ -15,6 +16,7 @@ public class TaskSheduling {
             this.deadline = deadline;
         }
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
@@ -35,29 +37,73 @@ public class TaskSheduling {
         }
 
         int[] delays = new int[maxDeadline + 1];
-        int[] toUse = new int[maxDeadline + 1];
+        Arrays.fill(delays, Integer.MIN_VALUE);
+
         for (int i = 0; i < t; i++) {
             delays[shed[i].deadline] = -shed[i].deadline;
-            toUse[shed[i].deadline] = 1;
         }
 
+        int[] tree = new int[4 * (maxDeadline + 1)];
+        buildSegTree(delays, tree, 1, 0, maxDeadline);
+        printTree(tree, 1, 0, maxDeadline);
+
         for (int i = 0; i < t; i++) {
-            Task task = shed[i];
-            for (int j = task.deadline; j <= maxDeadline; j++) {
-                if (toUse[j] == 1) {
-                    delays[j] += task.duration;
-                }
-            }
+            updateInterval(tree, 1, 0, maxDeadline, shed[i].deadline, maxDeadline, shed[i].duration);
+            System.out.println(tree[1]);
+            printTree(tree, 1, 0, maxDeadline);
+        }
+    }
 
-            int res = 0;
+    private static void printTree(int[] tree, int v, int left, int right) {
+        if (left == right) {
+            System.out.println("[" + left + " - " + right + "] = " + tree[v]);
+        } else {
+            int mid = (left + right) / 2;
+            printTree(tree, 2 * v, left, mid);
+            printTree(tree, 2 * v + 1, mid + 1, right);
+            System.out.println("[" + left + " - " + right + "] = " + tree[v]);
+        }
+    }
 
-            for (int j = 0; j <= maxDeadline; j++) {
-                if (toUse[j] == 1 && delays[j] > res) {
-                    res = delays[j];
-                }
-            }
+    private static void buildSegTree(int[] delays, int[] tree, int v, int left, int right) {
+        if (left == right) {
+            tree[v] = delays[left];
+            //System.out.println("[" + left + " - " + right + "] = " + tree[v]);
+        } else {
+            int mid = (left + right) / 2;
+            buildSegTree(delays, tree, 2 * v, left, mid);
+            buildSegTree(delays, tree, 2 * v + 1, mid + 1, right);
+            tree[v] = Math.max(tree[2 * v], tree[2 * v + 1]);
+            //System.out.println("[" + left + " - " + right + "] = " + tree[v]);
+        }
+    }
 
-            System.out.println(res);
+    private static void updateInterval(int[] tree, int v, int l, int r, int intL, int intR, int incr) {
+        if (intL > intR) {
+            return;
+        }
+
+        if (l == intL && r == intR) {
+            tree[v] += incr;
+        } else {
+            int mid = (l + r) / 2;
+            updateInterval(tree, v * 2,        l,       mid, intL,                    Math.min(mid, intR), incr);
+            updateInterval(tree, v * 2 + 1, mid + 1, r,   Math.max(mid + 1, intL), intR,                incr);
+            tree[v] = Math.max(tree[2 * v], tree[2 * v + 1]);
+        }
+    }
+
+    private static int getMaxValue(int[] tree, int v, int l, int r, int intL, int intR) {
+        if (intL > intR) {
+            return Integer.MIN_VALUE;
+        }
+
+        if (l == intL && r == intR) {
+            return tree[v];
+        } else {
+            int mid = (l + r) / 2;
+            return Math.max(getMaxValue(tree, 2 * v, l, mid, intL, Math.min(mid, intR)),
+                    getMaxValue(tree, 2 * v + 1, mid + 1, r, Math.max(mid + 1, intL), intR));
         }
     }
 }
