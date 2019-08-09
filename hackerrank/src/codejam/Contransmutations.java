3c1a514c248d8f79c0f54c4a00088bf2a0c1c9d3
Denis Mironchuk <denis_mironchuk@epam.com>
2019-08-09 12:14:46 +0300

    contransmutation cycle collapse logic

--- hackerrank/src/codejam/Contransmutations.java
+++ hackerrank/src/codejam/Contransmutations.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.Map.Entry;
 
 /*
 test data
@@ -20,6 +21,8 @@ test data
  */
 
 public class Contransmutations {
+    private static final int MOD = 1000000007;
+
     private static int m;
     private static Node[] metals;
     private static int[] grams;
@@ -77,13 +80,77 @@ public class Contransmutations {
             removeEmptyMetals();
 
             if (metals[1] == null) {
-                //0 value can be obtained
+                System.out.printf("Case #%s: %s\n", t, 0);
                 return;
             } else {
-                Set<Node> cycle = getCycle(metals[1], new int[m + 1], new LinkedList<>());
-                System.out.println(cycle);
+                Map<Node, Node> cycle = getCycle(metals[1], new int[m + 1], new LinkedList<>());
+                if (cycle == null) {
+                    //calculate amount
+                } else {
+                    if (!cycle.containsKey(metals[1])) {
+                        System.out.printf("Case #%s: %s\n", t, "UNBOUNDED");
+                    } else {
+                        collapseCycle(cycle);
+                        Map<Node, Node> cycle2 = getCycle(metals[1], new int[m + 1], new LinkedList<>());
+                        if (null == cycle2) {
+                            //calculate amount
+                        } else {
+                            System.out.printf("Case #%s: %s\n", t, "UNBOUNDED");
+                        }
+                    }
+                }
             }
         }
+    }
+
+    private static void collapseCycle(Map<Node, Node> cycle) {
+        int weight = 0;
+        Node initNode = new Node(1);
+
+        for (Entry<Node, Node> entry : cycle.entrySet()) {
+            Node cycleNode = entry.getKey();
+            Node outgoingNode = entry.getValue();
+
+            for (Node in : outgoingNode.ins) {
+                if (in != cycleNode) {
+                    Node toProc = cycle.containsKey(in) ? initNode : in;
+                    toProc.addOutNode(initNode);
+                }
+            }
+
+            for (Node out : cycleNode.outs) {
+                if (out != cycleNode) {
+                    Node toProc = cycle.containsKey(out) ? initNode : out;
+                    initNode.addOutNode(toProc);
+                }
+            }
+
+            metals[outgoingNode.num] = null;
+            weight = (weight + grams[outgoingNode.num]) % MOD;
+        }
+
+        for (Node nd : metals) {
+            if (nd != null) {
+                Iterator<Node> insItr = nd.ins.iterator();
+                while (insItr.hasNext()) {
+                    Node in = insItr.next();
+                    if (cycle.containsKey(in)) {
+                        insItr.remove();
+                    }
+                }
+
+                Iterator<Node> outsItr = nd.outs.iterator();
+                while (outsItr.hasNext()) {
+                    Node out = outsItr.next();
+                    if (cycle.containsKey(out)) {
+                        outsItr.remove();
+                    }
+                }
+            }
+        }
+
+        metals[1] = initNode;
+        grams[1] = weight;
     }
 
     private static void removeNonReachingFirst() {
@@ -140,12 +207,13 @@ public class Contransmutations {
         }
     }
 
-    private static Set<Node> getCycle(Node nd, int[] processed, LinkedList<Node> path) {
+    //returns list or edges represented as outgoing node -> ingoing node
+    private static Map<Node, Node> getCycle(Node nd, int[] processed, LinkedList<Node> path) {
         processed[nd.num] = 1;
         path.add(nd);
 
         for (Node out : nd.outs) {
-            Set<Node> cycle = null;
+            Map<Node, Node> cycle = null;
 
             if (processed[out.num] == 0) {
                 cycle = getCycle(out, processed, path);
@@ -164,15 +232,19 @@ public class Contransmutations {
         return null;
     }
 
-    private static Set<Node> restoreCycleNodes(LinkedList<Node> path, Node out) {
-        Set<Node> cycle = new HashSet<>();
-        cycle.add(out);
+    private static Map<Node, Node> restoreCycleNodes(LinkedList<Node> path, Node out) {
+        Map<Node, Node> cycle = new HashMap<>();
 
         Iterator<Node> itr = path.descendingIterator();
+        Node outgoing = out;
 
         for (Node nd = itr.next(); nd != out; nd = itr.next()) {
-            cycle.add(nd);
+            cycle.put(nd, outgoing);
+            outgoing = nd;
         }
+
+        cycle.put(out, outgoing);
+
         return cycle;
     }
 }
