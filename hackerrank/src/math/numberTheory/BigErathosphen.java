a251d4acd1486cc5cfa18ca4f2f44b68a61ebaac
Denis Mironchuk <denis_mironchuk@epam.com>
2020-01-11 22:04:01 +0300

    nice clique

--- hackerrank/src/math/numberTheory/BigErathosphen.java
+++ hackerrank/src/math/numberTheory/BigErathosphen.java
@@ -4,23 +4,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.Map.Entry;
 
 public class BigErathosphen {
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             int n = Integer.parseInt(br.readLine());
             long[] d = new long[n];
-            StringTokenizer nTkn = new StringTokenizer(br.readLine());
+            /*StringTokenizer nTkn = new StringTokenizer(br.readLine());
             long maxD = -1;
             for (int i = 0; i < n; i++) {
                 d[i] = Long.parseLong(nTkn.nextToken());
                 if (d[i] > maxD) {
                     maxD = d[i];
                 }
+            }*/
+
+            long maxD = -1;
+            for (int i = 0; i < n; i++) {
+                d[i] = 1000000000000000l;
+                if (d[i] > maxD) {
+                    maxD = d[i];
+                }
             }
 
-            //Date start = new Date();
+            Date d1 = new Date();
             int nn = (int)Math.sqrt(maxD) + 1;
             int[] isPrime = new int[nn + 1];
             Arrays.fill(isPrime, 1);
@@ -32,6 +39,9 @@ public class BigErathosphen {
                 }
             }
 
+            Date d2 = new Date();
+
+
             List<Long> primes = new ArrayList<>();
             for (int i = 2; i <= nn; i++) {
                 if (isPrime[i] == 1) {
@@ -39,52 +49,60 @@ public class BigErathosphen {
                 }
             }
 
-            Map<Long, Integer>[] factor = new Map[n];
-
-            for (int i = 0; i < n; i++) {
-                factor[i] = new HashMap<>();
-                long di = d[i];
-                for (Long prime : primes) {
-                    while (di % prime == 0) {
-                        factor[i].merge(prime, 1, (oldVal, nevVal) -> oldVal + 1);
-                        di /= prime;
-                    }
-                }
-                if (di != 1) {
-                    factor[i].put(di, 1);
-                }
-            }
+            Date d3 = new Date();
 
             int[] parity1 = new int[n];
-
             int par1Cnt = 0;
-            for (int i = 0; i < n; i++) {
-                Map<Long, Integer> map = factor[i];
-                parity1[i] = map.keySet().size() % 2;
-                par1Cnt += parity1[i];
-            }
 
             int[] parity2 = new int[n];
             int par2Cnt = 0;
 
+            long itrCnt = 0;
+
             for (int i = 0; i < n; i++) {
-                Map<Long, Integer> map = factor[i];
+                long di = d[i];
+                int primePow;
+                int distPrimes = 0;
                 parity2[i] = 1;
-                for (Entry<Long, Integer> entry : map.entrySet()) {
-                    if (entry.getKey() != 2 && entry.getValue() % 2 == 1) {
+
+                for (Long prime : primes) {
+                    itrCnt++;
+                    primePow = 0;
+                    boolean isDiv = di % prime == 0;
+                    if (isDiv) {
+                        distPrimes++;
+                    }
+
+                    while (di % prime == 0) {
+                        primePow++;
+                        di /= prime;
+                    }
+
+                    if (isDiv && prime != 2 && primePow % 2 == 1) {
                         parity2[i] = 0;
-                        break;
                     }
                 }
-                par2Cnt+=parity2[i];
+
+                if (di != 1) {
+                    distPrimes++;
+                    if (di != 2) {
+                        parity2[i] = 0;
+                    }
+                }
+
+                parity1[i] = distPrimes % 2;
+                par1Cnt += parity1[i];
+                par2Cnt += parity2[i];
             }
 
             int[] resCands = new int[] {par1Cnt, n - par1Cnt, par2Cnt, n - par2Cnt};
             Arrays.sort(resCands);
-
-            //Date end = new Date();
-            //System.out.println(end.getTime() - start.getTime() + "ms");
+            Date d4 = new Date();
             System.out.println(resCands[3]);
+            System.out.println(d2.getTime() - d1.getTime() + "ms");
+            System.out.println(d3.getTime() - d2.getTime() + "ms");
+            System.out.println(d4.getTime() - d3.getTime() + "ms");
+            System.out.println("itrCnt = " + itrCnt);
         }
     }
 }
