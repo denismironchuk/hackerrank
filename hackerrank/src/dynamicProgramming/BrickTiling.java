c4b5ccc25cefd758599922224e5e0cff895601c6
Denis Mironchuk <dmiro@softserveinc.com>
2020-03-13 17:24:02 +0200

    brick tiling

--- hackerrank/src/dynamicProgramming/BrickTiling.java
+++ hackerrank/src/dynamicProgramming/BrickTiling.java
@@ -12,7 +12,8 @@ import java.util.StringTokenizer;
 
 public class BrickTiling {
     private static abstract class BaseTile {
-        public abstract int getBottomLen();
+        public abstract int getLen();
+        public abstract int getHeight();
         public abstract int[] getUpPositionMasks(int bottomRightPos, int columns);
         public abstract int getStartIndex();
         public abstract int getEndIndex(int cols);
@@ -24,8 +25,13 @@ public class BrickTiling {
           .###.*/
 
         @Override
-        public int getBottomLen() {
+        public int getLen() {
             return 3;
+        }
+
+        @Override
+        public int getHeight() {
+            return 2;
         }
 
         @Override
@@ -55,8 +61,13 @@ public class BrickTiling {
           .###.*/
 
         @Override
-        public int getBottomLen() {
+        public int getLen() {
             return 3;
+        }
+
+        @Override
+        public int getHeight() {
+            return 2;
         }
 
         @Override
@@ -86,8 +97,13 @@ public class BrickTiling {
           .#...*/
 
         @Override
-        public int getBottomLen() {
+        public int getLen() {
             return 1;
+        }
+
+        @Override
+        public int getHeight() {
+            return 3;
         }
 
         @Override
@@ -117,8 +133,13 @@ public class BrickTiling {
           ..#..*/
 
         @Override
-        public int getBottomLen() {
+        public int getLen() {
             return 1;
+        }
+
+        @Override
+        public int getHeight() {
+            return 3;
         }
 
         @Override
@@ -148,8 +169,13 @@ public class BrickTiling {
           .##..*/
 
         @Override
-        public int getBottomLen() {
+        public int getLen() {
             return 2;
+        }
+
+        @Override
+        public int getHeight() {
+            return 3;
         }
 
         @Override
@@ -179,8 +205,13 @@ public class BrickTiling {
           .##..*/
 
         @Override
-        public int getBottomLen() {
+        public int getLen() {
             return 2;
+        }
+
+        @Override
+        public int getHeight() {
+            return 3;
         }
 
         @Override
@@ -204,99 +235,138 @@ public class BrickTiling {
         }
     }
 
+    private static class Tile7 extends BaseTile {
+        /*.....
+          .###.
+          .#...*/
+
+        @Override
+        public int getLen() {
+            return 3;
+        }
+
+        @Override
+        public int getHeight() {
+            return 2;
+        }
+
+        @Override
+        public int[] getUpPositionMasks(int bottomRightPos, int columns) {
+            return new int[] {
+                    1 << (bottomRightPos),
+                    1 << (bottomRightPos + columns),
+                    1 << (bottomRightPos + columns + 1),
+                    1 << (bottomRightPos + columns + 2),
+            };
+        }
+
+        @Override
+        public int getStartIndex() {
+            return 1;
+        }
+
+        @Override
+        public int getEndIndex(int cols) {
+            return cols - 3;
+        }
+    }
+
+    private static class Tile8 extends BaseTile {
+        /*.....
+          .###.
+          ...#.*/
+
+        @Override
+        public int getLen() {
+            return 3;
+        }
+
+        @Override
+        public int getHeight() {
+            return 2;
+        }
+
+        @Override
+        public int[] getUpPositionMasks(int bottomRightPos, int columns) {
+            return new int[] {
+                    1 << (bottomRightPos),
+                    1 << (bottomRightPos + columns),
+                    1 << (bottomRightPos + columns - 1),
+                    1 << (bottomRightPos + columns - 2),
+            };
+        }
+
+        @Override
+        public int getStartIndex() {
+            return 2;
+        }
+
+        @Override
+        public int getEndIndex(int cols) {
+            return cols - 1;
+        }
+    }
+
+    private static int n;
+    private static int m;
+    private static char[][] board;
+
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             int T = Integer.parseInt(br.readLine());
-            List<BaseTile> tiles = Arrays.asList(new Tile1(), new Tile2(), new Tile3(), new Tile4(), new Tile5(), new Tile6());
+            List<BaseTile> tiles = Arrays.asList(new Tile1(), new Tile2(), new Tile3(), new Tile4(),
+                    new Tile5(), new Tile6(), new Tile7(), new Tile8());
 
             for (int t = 0; t < T; t++) {
                 StringTokenizer sizeTkn = new StringTokenizer(br.readLine());
-                int n = Integer.parseInt(sizeTkn.nextToken());
-                int m = Integer.parseInt(sizeTkn.nextToken());
-                char[][] board = new char[n][m];
+                n = Integer.parseInt(sizeTkn.nextToken());
+                m = Integer.parseInt(sizeTkn.nextToken());
+                board = new char[n][m];
 
                 for (int i = 0; i < n; i++) {
                     board[i] = br.readLine().toCharArray();
                 }
 
-                LinkedList<Map<Integer, Long>> lastStates = new LinkedList<>();
+                LinkedList<Map<Integer, Long>> columnStates = new LinkedList<>();
+                columnStates.add(new HashMap<>());
+                columnStates.add(new HashMap<>());
+                Map<Integer, Long> initStates = new HashMap<>();
+                columnStates.add(initStates);
 
-                for (int row = 0; row < n; row++) {
-                    Map<Integer, Long> prevFree = new HashMap<>();
-
-                    if (row == 0) {
-                        lastStates.clear();
-                        lastStates.add(new HashMap<>());
-                        lastStates.add(new HashMap<>());
-                        lastStates.add(prevFree);
-
-                        int mul = 1;
-                        int initState = 0;
-                        for (int row1 = 0; row1 < 3; row1++) {
-                            for (int col1 = 0; col1 < m; col1++) {
-                                if (board[row1][col1] == '#') {
-                                    initState += mul;
-                                }
-                                mul <<= 1;
-                            }
+                int mul = 1 << m;
+                int initState = 0;
+                for (int row1 = 0; row1 < 2; row1++) {
+                    for (int col1 = 0; col1 < m; col1++) {
+                        if (board[row1][col1] == '#') {
+                            initState += mul;
                         }
-
-                        prevFree.put(initState, 1l);
-                    } else {
-                        Map<Integer, Long> last = lastStates.pollLast();
-                        lastStates.clear();
-                        lastStates.add(new HashMap<>());
-                        lastStates.add(new HashMap<>());
-                        lastStates.add(prevFree);
-
-                        int lastRow = 0;
-                        int mul = 1;
-
-                        for (int col = 0; col < m; col++) {
-                            if (board[row][col] == '#') {
-                                lastRow += mul;
-                            }
-                            mul *= 2;
-                        }
-
-                        lastRow <<= 2 * m;
-
-                        for (Map.Entry<Integer, Long> entry : last.entrySet()) {
-                            Integer state = entry.getKey();
-                            Long count = entry.getValue();
-
-                            state >>= m;
-                            state += lastRow;
-
-                            prevFree.put(state, count);
-                        }
+                        mul <<= 1;
                     }
+                }
+
+                initStates.put(initState, 1l);
+
+                for (int row = 0; row < n - 1; row++) {
+                    addNewRowToStates(columnStates, row);
 
                     for (int col = 0; col < m; col++) {
                         Map<Integer, Long> newStates = new HashMap<>();
 
+                        if (board[row][col] == '#') {
+                            newStates.putAll(columnStates.peekLast());
+                        }
+
                         for (BaseTile tile : tiles) {
-                            boolean skip = false;
-                            int bottomLen = tile.getBottomLen();
-
-                            for (int prevPos = 0; prevPos < bottomLen; prevPos++) {
-                                if (col < tile.getStartIndex() || col > tile.getEndIndex(m) || board[row][col - prevPos] == '#') {
-                                    skip = true;
-                                    break;
-                                }
-                            }
-
-                            if (skip) {
+                            if (col < tile.getStartIndex() || col > tile.getEndIndex(m) || row + tile.getHeight() > n) {
                                 continue;
                             }
 
-                            Map<Integer, Long> statesToCheck = lastStates.get(3 - bottomLen);
-
-                            for (Map.Entry<Integer, Long> entry : statesToCheck.entrySet()) {
+                            Map<Integer, Long> states = columnStates.get(3 - tile.getLen());
+                            for (Map.Entry<Integer, Long> entry : states.entrySet()) {
                                 Integer state = entry.getKey();
                                 Long cnt = entry.getValue();
 
-                                skip = false;
+                                boolean skip = false;
 
                                 for(int mask : tile.getUpPositionMasks(col, m)) {
                                     if ((state | mask) == state) {
@@ -314,12 +384,46 @@ public class BrickTiling {
                                 newStates.merge(state, cnt, (oldVal, newVal) -> oldVal + newVal);
                             }
                         }
-                    }
 
-                    lastStates.add(prevFree);
-                    lastStates.pollFirst();
+                        columnStates.add(newStates);
+                        columnStates.pollFirst();
+                    }
                 }
             }
         }
+    }
+
+    private static void addNewRowToStates(LinkedList<Map<Integer, Long>> columnStates, int row) {
+        Map<Integer, Long> rowInitStates = new HashMap<>();
+
+        int lastRow = 0;
+
+        if (row < n - 2) {
+            int mul = 1;
+
+            for (int col = 0; col < m; col++) {
+                if (board[row + 2][col] == '#') {
+                    lastRow += mul;
+                }
+                mul *= 2;
+            }
+
+            lastRow <<= 2 * m;
+        }
+
+        for (Map.Entry<Integer, Long> entry : columnStates.pollLast().entrySet()) {
+            Integer state = entry.getKey();
+            Long count = entry.getValue();
+
+            state >>= m;
+            state += lastRow;
+
+            rowInitStates.put(state, count);
+        }
+
+        columnStates.clear();
+        columnStates.add(new HashMap<>());
+        columnStates.add(new HashMap<>());
+        columnStates.add(rowInitStates);
     }
 }
