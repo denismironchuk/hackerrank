a7aad713bd2601f5d2931a4a93e49fccd52ef5d6
Denis Mironchuk <dmiro@softserveinc.com>
2021-01-31 23:30:45 +0200

    kickstart 2020. toys. almost final solution

--- hackerrank/src/kickstart/Toys.java
+++ hackerrank/src/kickstart/Toys.java
@@ -1,5 +1,13 @@
 package kickstart;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Toys {
 
     private static class Toy implements Comparable<Toy> {
@@ -25,7 +33,10 @@ public class Toys {
 
         @Override
         public String toString() {
-            return position + ", " + value;
+            return "{" +
+                    "\"position\":" + position +
+                    ", \"value\":" + value +
+                    '}';
         }
     }
 
@@ -38,6 +49,8 @@ public class Toys {
         private Treap right;
 
         private Toy minPositionToy;
+
+        private Treap parent;
 
         public Treap(Toy toy) {
             this.toy = toy;
@@ -53,12 +66,52 @@ public class Toys {
             return merge2;
         }
 
+        public void updateMinPositionToyToTop() {
+            updateMinPositionToy();
+            if (parent != null) {
+                parent.updateMinPositionToyToTop();
+            }
+        }
+
+        public void updateMinPositionToy() {
+            this.minPositionToy = this.toy;
+            if (this.right != null) {
+                if (this.minPositionToy.position > this.right.minPositionToy.position) {
+                    this.minPositionToy = this.right.minPositionToy;
+                }
+            }
+
+            if (this.left != null) {
+                if (this.minPositionToy.position > this.left.minPositionToy.position) {
+                    this.minPositionToy = this.left.minPositionToy;
+                }
+            }
+        }
+
+        public void setLeft(Treap left) {
+            this.left = left;
+            if (this.left != null) {
+                this.left.parent = this;
+            }
+        }
+
+        public void setRight(Treap right) {
+            this.right = right;
+            if (this.right != null) {
+                this.right.parent = this;
+            }
+        }
+
         public Treap[] split(Toy splitToy) {
             if (toy.compareTo(splitToy) == -1) {
                 if (this.right != null) {
                     Treap[] splitRes = this.right.split(splitToy);
-                    this.right = splitRes[0];
+                    this.setRight(splitRes[0]);
                     splitRes[0] = this;
+                    this.updateMinPositionToy();
+                    if (splitRes[1] != null) {
+                        splitRes[1].updateMinPositionToy();
+                    }
                     return splitRes;
                 } else {
                     return new Treap[] {this, null};
@@ -66,8 +119,12 @@ public class Toys {
             } else {
                 if (this.left != null) {
                     Treap[] splitRes = this.left.split(splitToy);
-                    this.left = splitRes[1];
+                    this.setLeft(splitRes[1]);
                     splitRes[1] = this;
+                    this.updateMinPositionToy();
+                    if (splitRes[0] != null) {
+                        splitRes[0].updateMinPositionToy();
+                    }
                     return splitRes;
                 } else {
                     return new Treap[] {null, this};
@@ -85,44 +142,237 @@ public class Toys {
             }
 
             if (smaller.rank > greater.rank) {
-                smaller.right = merge(smaller.right, greater);
+                smaller.setRight(merge(smaller.right, greater));
+                smaller.updateMinPositionToy();
                 return smaller;
             } else {
-                greater.left = merge(smaller, greater.left);
+                greater.setLeft(merge(smaller, greater.left));
+                greater.updateMinPositionToy();
                 return greater;
             }
         }
 
-        private int countNodes() {
-            int res = 1;
+        public Treap find(Toy toy) {
+            int compRes = toy.compareTo(this.toy);
+            if (compRes == 0) {
+                return this;
+            } else if (compRes == -1) {
+                if (this.left == null) {
+                    return null;
+                } else {
+                    return this.left.find(toy);
+                }
+            } else {
+                if (this.right == null) {
+                    return null;
+                } else {
+                    return this.right.find(toy);
+                }
+            }
+        }
+
+        public void delete(Toy toy) {
+            Treap searchNode = find(toy);
+            if (searchNode.left != null || searchNode.right != null) {
+                Treap nodeReplace = merge(searchNode.left, searchNode.right);
+                searchNode.toy = nodeReplace.toy;
+                searchNode.setRight(nodeReplace.right);
+                searchNode.setLeft(nodeReplace.left);
+                searchNode.minPositionToy = nodeReplace.minPositionToy;
+                searchNode.validateMinPositionToy();
+            } else {
+                if (searchNode.parent.left == searchNode) {
+                    searchNode.parent.left = null;
+                } else {
+                    searchNode.parent.right = null;
+                }
+            }
+
+            searchNode.updateMinPositionToyToTop();
+        }
+
+        public int getSize() {
+            int size = 1;
+
+            if (left != null) {
+                size += left.getSize();
+            }
+
+            if (right != null) {
+                size += right.getSize();
+            }
+
+            return size;
+        }
+
+        public void validateMinPositionToy() {
+            List<Toy> minPosToys = new ArrayList<>();
+            minPosToys.add(this.toy);
             if (this.left != null) {
-                res += this.left.countNodes();
+                this.left.validateMinPositionToy();
+                minPosToys.add(this.left.minPositionToy);
             }
-
             if (this.right != null) {
-                res += this.right.countNodes();
+                this.right.validateMinPositionToy();
+                minPosToys.add(this.right.minPositionToy);
             }
-
-            return res;
+            minPosToys.sort(Comparator.comparingInt(toy -> toy.position));
+            if (minPosToys.get(0) != this.minPositionToy) {
+                throw new RuntimeException();
+            }
         }
 
         @Override
         public String toString() {
-            return String.valueOf(this.countNodes());
+            return "{" +
+                    "\"toy\":" + toy +
+                    ", \"rank\":" + rank +
+                    ", \"left\":" + left +
+                    ", \"right\":" + right +
+                    '}';
         }
     }
 
-    public static void main(String[] args) {
-        Toy t1 = new Toy(1, (long)(20 * Math.random()));
-        Toy t2 = new Toy(2, (long)(20 * Math.random()));
-        Toy t3 = new Toy(3, (long)(20 * Math.random()));
-        Toy t4 = new Toy(4, (long)(20 * Math.random()));
-        Toy t5 = new Toy(5, (long)(20 * Math.random()));
-        Toy t6 = new Toy(6, (long)(20 * Math.random()));
-        Toy t7 = new Toy(7, (long)(20 * Math.random()));
-        Toy t8 = new Toy(8, (long)(20 * Math.random()));
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int T = Integer.parseInt(br.readLine());
+            for (int t = 1; t <= T; t++) {
+                int n = Integer.parseInt(br.readLine());
+                long[][] toyDescr = new long[n][2];
+                long roundTime = 0;
+                long[] playTime = new long[n];
+                for (int j = 0; j < n; j++) {
+                    StringTokenizer tkn = new StringTokenizer(br.readLine());
+                    toyDescr[j][0] = Long.parseLong(tkn.nextToken());
+                    toyDescr[j][1] = Long.parseLong(tkn.nextToken());
+                    roundTime += toyDescr[j][0];
+                    playTime[j] = toyDescr[j][0];
+                }
 
-        Treap treap = new Treap(t1).add(t2).add(t3).add(t4).add(t5).add(t6).add(t7).add(t8);
-        System.out.println();
+                calculateOptimal(t, n, toyDescr, roundTime, playTime);
+            }
+        }
+    }
+
+    private static void calculateTrivial(int t, int n, long[][] toyDescr, long roundTime, long[] playTime) {
+        int[] processed = new int[n];
+        int breakPos = 0;
+        for (;breakPos < n; breakPos++) {
+            if (roundTime - toyDescr[breakPos][0] < toyDescr[breakPos][1]) {
+                break;
+            }
+        }
+    }
+
+    private static void calculateOptimal(int t, int n, long[][] toyDescr, long roundTime, long[] playTime) {
+        Treap toys = new Treap(new Toy(0, roundTime - toyDescr[0][0] - toyDescr[0][1]));
+        for (int i = 1; i < n; i++) {
+            toys = toys.add(new Toy(i, roundTime - toyDescr[i][0] - toyDescr[i][1]));
+        }
+
+        long limit = 0;
+        List<Toy> deleteOrder = new ArrayList<>();
+        boolean infinityFound = false;
+        for (int i = 0; i < n - 1; i++) {
+            Treap[] splitRes = toys.split(new Toy(Integer.MIN_VALUE, limit));
+            if (splitRes[0] == null) {
+                infinityFound = true;
+                break;
+            } else {
+                Treap minPart = splitRes[0];
+                Toy toyToDelete = minPart.minPositionToy;
+                deleteOrder.add(toyToDelete);
+                limit += playTime[toyToDelete.position];
+                if (minPart.left == null && minPart.right == null) {
+                    toys = splitRes[1];
+                } else {
+                    minPart.delete(toyToDelete);
+                    toys = Treap.merge(minPart, splitRes[1]);
+                }
+            }
+        }
+
+        if (infinityFound) {
+            System.out.printf("Case #%s: %s INDEFINITELY\n", t, deleteOrder.size());
+        } else {
+            deleteOrder.add(toys.toy);
+            long maxTime = -1;
+            int maxTimeToys = -1;
+
+            for (int i = 0; i < n; i++) {
+                Toy toyToDelete = deleteOrder.get(i);
+                long maxTimeCandidate = roundTime;
+                for (int j = 0; j < toyToDelete.position; j++) {
+                    maxTimeCandidate += playTime[j];
+                }
+
+                if (maxTimeCandidate > maxTime) {
+                    maxTime = maxTimeCandidate;
+                    maxTimeToys = i;
+                }
+
+                roundTime -= playTime[toyToDelete.position];
+                playTime[toyToDelete.position] = 0;
+            }
+
+            System.out.printf("Case #%s: %s %s\n", t, maxTimeToys, maxTime);
+        }
+    }
+
+    private static void testTreap() {
+        int toysCnt = 10;
+        int maxValue = 5;
+        while (true) {
+            List<Toy> toys = new ArrayList<>();
+            for (int i = 0; i < toysCnt; i++) {
+                toys.add(new Toy(i, (long) (maxValue * Math.random())));
+            }
+
+            Treap treap = new Treap(toys.get(0));
+            treap.validateMinPositionToy();
+            for (int i = 1; i < toysCnt; i++) {
+                int initialSize = treap.getSize();
+                treap = treap.add(toys.get(i));
+                treap.validateMinPositionToy();
+                int sizeAfterInsert = treap.getSize();
+
+                if (null == treap.find(toys.get(i))) {
+                    throw new RuntimeException();
+                }
+
+                if (initialSize + 1 != sizeAfterInsert) {
+                    throw new RuntimeException();
+                }
+            }
+
+            System.out.println(treap);
+
+            while (treap.getSize() > 1) {
+                int initialSize = treap.getSize();
+                int indexToDelete = (int)(toys.size() * Math.random());
+                Toy toyToDelete = toys.get(indexToDelete);
+
+                toys.remove(indexToDelete);
+                System.out.println("================");
+                System.out.println(toyToDelete);
+
+                if (null == treap.find(toyToDelete)) {
+                    throw new RuntimeException();
+                }
+
+                treap.delete(toyToDelete);
+                treap.validateMinPositionToy();
+                System.out.println(treap);
+                int sizeAfterDelete = treap.getSize();
+
+                if (null != treap.find(toyToDelete)) {
+                    throw new RuntimeException();
+                }
+
+                if (initialSize - 1 != sizeAfterDelete) {
+                    throw new RuntimeException();
+                }
+            }
+        }
     }
 }
