f51032bdeb6553dd9d9b53cae65601508fe9f98f
Denis Mironchuk <dmiro@softserveinc.com>
2022-10-28 13:03:22 +0300

    codejam 2022. round 2. pixelated circle solution test set 2. final solution

--- hackerrank/src/codejam/year2022/round2/PixelatedCircle.java
+++ hackerrank/src/codejam/year2022/round2/PixelatedCircle.java
@@ -2,11 +2,25 @@ package codejam.year2022.round2;
 
 public class PixelatedCircle {
 
-    static int R = 9;
+    static int R = 4;
     static int[][] boardFull = new int[2 * R + 1][2 * R + 1];
     static int[][] boardGaps = new int[2 * R + 1][2 * R + 1];
 
     public static void main(String[] args) {
+        for (int x = 1; x <= R; x++) {
+            System.out.printf("%s - ", x);
+            double xD = x;
+            for (int r = x; r <= R; r++) {
+                double rD = r;
+                System.out.printf("%s ", Math.round(Math.sqrt(rD * rD - xD * xD)));
+            }
+            double rD = R;
+            int maxHeight = (int)Math.round(Math.sqrt(rD * rD - xD * xD));
+            double maxHeightD = maxHeight;
+            System.out.println();
+            //System.out.println(Math.round(Math.sqrt(maxHeight * maxHeight + xD * xD)));
+        }
+
         for (int r = 0; r <= R; r++) {
             draw_circle_perimeter(r);
         }
@@ -19,9 +33,9 @@ public class PixelatedCircle {
             double fX = x;
             int y = (int) Math.round(Math.sqrt(fRadius * fRadius - fX * fX));
             boardGaps[x + R][y + R] = 1;
-            boardGaps[x + R][-y + R] = 1;
-            boardGaps[y + R][x + R] = 1;
-            boardGaps[-y + R][x + R] = 1;
+            //boardGaps[x + R][-y + R] = 1;
+            //boardGaps[y + R][x + R] = 1;
+            //boardGaps[-y + R][x + R] = 1;
         }
     }
 
--- hackerrank/src/codejam/year2022/round2/PixelatedCircleSolution.java
+++ hackerrank/src/codejam/year2022/round2/PixelatedCircleSolution.java
@@ -10,44 +10,53 @@ public class PixelatedCircleSolution {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             int T = Integer.parseInt(br.readLine());
             for (int t = 1; t <= T; t++) {
-                int R = Integer.parseInt(br.readLine());
-                int[][] fullBoard = new int[2 * R + 1][2 * R + 1];
-                for (int x = -R; x <= R; x++) {
-                    for (int y = -R; y <= R; y++) {
-                        double xD = x;
-                        double yD = y;
-                        if (Math.round(Math.sqrt(xD * xD + yD * yD)) <= R) {
-                            fullBoard[x + R][y + R] = 1;
-                        }
+                long R = Long.parseLong(br.readLine());
+                long diagMismatch = 0;
+                long misMatch = 0;
+                long x = 1;
+                for (x = 1; 2 * x * x <= R * R; x++) {
+                    double xD = x;
+                    double rD = R;
+                    long maxY = Math.round(Math.sqrt(rD * rD - xD * xD));
+                    //total mismatch in a row
+                    long inc = (maxY + 1) - (R + 1 - x);
+                    misMatch += inc;
+
+                    //mismatch on diagonal
+                    long diagRadius = Math.round(Math.sqrt(xD * xD + xD * xD));
+                    double diagRadiusD = diagRadius;
+                    if (Math.round(Math.sqrt(diagRadiusD * diagRadiusD - xD * xD)) != x
+                            && Math.round(Math.sqrt((diagRadiusD + 1) * (diagRadiusD + 1) - xD * xD)) != x
+                            && Math.round(Math.sqrt((diagRadiusD - 1) * (diagRadiusD - 1) - xD * xD)) != x) {
+                        diagMismatch += 1;
+                        misMatch -= 1;
+                    }
+
+                    //mismatch under diagonal
+                    long maxRadiusUnderDiagonal = -1;
+                    if (Math.round(Math.sqrt((diagRadiusD + 1) * (diagRadiusD + 1) - xD * xD)) < x) {
+                        maxRadiusUnderDiagonal = diagRadius + 1;
+                    } else if (Math.round(Math.sqrt(diagRadiusD * diagRadiusD - xD * xD)) < x) {
+                        maxRadiusUnderDiagonal = diagRadius;
+                    } else {
+                        maxRadiusUnderDiagonal = diagRadius - 1;
+                    }
+
+                    long underDiagonalMismatch = (x - 1 + 1) - (maxRadiusUnderDiagonal + 1 - x);
+                    misMatch -= underDiagonalMismatch;
+
+                    //upper element mismatch
+                    if (maxY + 1 > x && Math.round(Math.sqrt(x * x + (maxY + 1) * (maxY + 1))) <= R) {
+                        misMatch += 1;
                     }
                 }
 
-                int[][] radiusBoard = new int[2 * R + 1][2 * R + 1];
-                for (int r = 0; r <= R; r++) {
-                    for (int x = 0; x <= r; x++) {
-                        double xD = x;
-                        double rD = r;
-                        int y = (int) Math.round(Math.sqrt(rD * rD - xD * xD));
-                        radiusBoard[x + R][y + R] = 1;
-                    }
+                if (Math.round(Math.sqrt(R * R - x * x)) != x && Math.round(Math.sqrt(2 * x * x)) <= R) {
+                    diagMismatch += 1;
                 }
-                int res = 0;
-                int diagMisMatch = 0;
-                for (int x = 0; x <= R; x++) {
-                    if (fullBoard[x + R][x + R] != radiusBoard[x + R][x + R]) {
-                        diagMisMatch++;
-                    }
-                }
-                for (int x = 0; x <= R; x++) {
-                    for (int y = x + 1; y <= R; y++) {
-                        if (fullBoard[x + R][y + R] != radiusBoard[x + R][y + R]) {
-                            res++;
-                        }
-                    }
-                }
-                System.out.printf("Case #%s: %s\n", t, 8 * res + 4 * diagMisMatch);
+
+                System.out.printf("Case #%s: %s\n", t, 8 * misMatch + 4 * diagMismatch);
             }
         }
     }
-
 }
