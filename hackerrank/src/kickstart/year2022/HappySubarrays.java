3d488c25fbaecc6d073ba8b43d36281e0edcaab9
Denis Mironchuk <dmiro@softserveinc.com>
2023-04-17 21:59:45 +0300

    kickstart 2022. round g. happy subarrays. attempt 1

--- hackerrank/src/kickstart/year2022/HappySubarrays.java
+++ hackerrank/src/kickstart/year2022/HappySubarrays.java
@@ -11,36 +11,45 @@ public class HappySubarrays {
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             int T = Integer.parseInt(br.readLine());
-            for (int t = 0; t < T; t++) {
+            for (int t = 1; t <= T; t++) {
                 int n = Integer.parseInt(br.readLine());
-                int[] a = new int[n];
+                long[] a = new long[n];
                 StringTokenizer tkn = new StringTokenizer(br.readLine());
                 for (int i = 0; i < n; i++) {
-                    a[i] = Integer.parseInt(tkn.nextToken());
+                    a[i] = Long.parseLong(tkn.nextToken());
                 }
-                int[] maxLenHappy = new int[n];
+                long[] maxLenHappy = new long[n];
+                long[] sums = new long[n];
+                long[] happySubArraySums = new long[n];
+
                 maxLenHappy[n - 1] = a[n - 1] < 0 ? 0 : 1;
+                sums[n - 1] = a[n - 1] < 0 ? 0 : a[n - 1];
+                happySubArraySums[n - 1] = sums[n - 1];
+                long res = happySubArraySums[n - 1];
+
                 for (int i = n - 2; i >= 0; i--) {
-                    if (a[i] < 0) {
-                        maxLenHappy[i] = 0;
-                    } else {
-                        int sum = 0;
+                    if (a[i] >= 0) {
                         int j = i;
-                        for (; sum + a[j] >= 0 && maxLenHappy[j] == 0; j++) {
-                            sum += a[j];
-                            maxLenHappy[i]++;
-                        }
-                        if (maxLenHappy[j] != 0) {
-                            maxLenHappy[i] += maxLenHappy[j];
-                            j += maxLenHappy[j] + 1;
-                            for (; j < n && sum + a[j] >= 0; j++) {
-                                sum += a[j];
+                        while (true) {
+                            for (; j < n && sums[i] + a[j] >= 0 && maxLenHappy[j] == 0; j++) {
+                                sums[i] += a[j];
                                 maxLenHappy[i]++;
+                                happySubArraySums[i] += sums[i];
+                            }
+                            if (j < n && maxLenHappy[j] != 0) {
+                                maxLenHappy[i] += maxLenHappy[j];
+                                happySubArraySums[i] += sums[i] * maxLenHappy[j] + happySubArraySums[j];
+                                sums[i] += sums[j];
+                                j += maxLenHappy[j] + 1;
+                            } else {
+                                break;
                             }
                         }
+                        res += happySubArraySums[i];
                     }
                 }
-                Arrays.stream(maxLenHappy).forEach(i -> System.out.printf("%s ", i));
+                //Arrays.stream(maxLenHappy).forEach(i -> System.out.printf("%s ", i));
+                System.out.printf("Case #%s: %s\n", t, res);
             }
         }
     }
